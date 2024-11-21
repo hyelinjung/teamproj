3743ae6eca8e826ee25784a4c53a5476e5b9a121
Taekyun131 <stk131@naver.com>
2024-11-21 18:19:26 +0900

    241121_1820_board

---
+++ src/main/java/com/asklepios/hospitalreservation_asklepios/boardRepository/IF_BoardRepository.java
@@ -0,0 +1,9 @@
+package com.asklepios.hospitalreservation.boardRepository;
+
+import com.asklepios.hospitalreservation.boardVO.BoardVO;
+
+
+
+public interface IF_BoardRepository {
+    public void insertBoard(BoardVO boardVO);
+}
