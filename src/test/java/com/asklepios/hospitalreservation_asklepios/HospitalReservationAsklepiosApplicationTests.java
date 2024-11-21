6ebfcd653b3560f25808d9872b1f130631e40a04
Jungon Jo <jojungon@gmail.com>
2024-11-21 09:25:36 +0900

    start Final Project

---
+++ src/test/java/com/asklepios/hospitalreservation_asklepios/HospitalReservationAsklepiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asklepios.hospitalreservation_asklepios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalReservationAsklepiosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
