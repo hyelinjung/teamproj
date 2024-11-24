1b34a618348b1bc11954d03f7f087bb56be98bc7
Taekyun131 <stk131@naver.com>
2024-11-24 17:40:23 +0900

    241124_1740_paging 수정중

--- src/main/java/com/asklepios/hospitalreservation_asklepios/Service/IF_BoardService.java
+++ src/main/java/com/asklepios/hospitalreservation_asklepios/Service/IF_BoardService.java
@@ -1,10 +1,15 @@
 package com.asklepios.hospitalreservation_asklepios.Service;
 
 import com.asklepios.hospitalreservation_asklepios.VO.BoardVO;
+import com.asklepios.hospitalreservation_asklepios.VO.PageVO;
 
 import java.util.List;
 
 public interface IF_BoardService {
-    public void addBoard(BoardVO boardVO);
-    public List<BoardVO> boardList();
+    public void addBoard(BoardVO boardVO) throws Exception;
+    public List<BoardVO> boardHealthList(PageVO pagevo)throws Exception;
+    public List<BoardVO> boardCampaignList(PageVO pagevo)throws Exception;
+    public List<BoardVO> boardMedList(PageVO pagevo)throws Exception;
+    public List<BoardVO> boardFreeList(PageVO pagevo)throws Exception;
+    public int boardCount();
 }
