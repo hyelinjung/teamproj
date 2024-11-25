5c335207b13dacc198be55e2b8e9fbb3769ce74e
NAGABARA <kayc321@nate.com>
2024-11-25 09:50:38 +0900

    241125 0925 vaccine view template

--- src/main/java/com/asklepios/hospitalreservation_asklepios/Controller/VaccineController.java
+++ src/main/java/com/asklepios/hospitalreservation_asklepios/Controller/VaccineController.java
@@ -1,4 +1,4 @@
-package com.asklepios.hospitalreservation_asklepios;
+package com.asklepios.hospitalreservation_asklepios.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
