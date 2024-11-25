159c95ff77135cdefdcdee2647bdc78cc8f255d7
Jeong Jiyong <98jiyong@gmail.com>
2024-11-25 17:55:02 +0900

    20241125_1754_클론 후 푸쉬

--- src/main/java/com/asklepios/hospitalreservation_asklepios/Controller/AsklepiosController.java
+++ src/main/java/com/asklepios/hospitalreservation_asklepios/Controller/AsklepiosController.java
@@ -10,4 +10,9 @@ public class AsklepiosController {
     public String home() {
         return "home";
     }
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
 }
