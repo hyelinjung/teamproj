81d6f8ff04c2dc8752159a76a24cc3bdaa16a25b
Taekyun131 <stk131@naver.com>
2024-11-25 17:59:56 +0900

    241125_1800_게시판페이징구현

--- src/main/java/com/asklepios/hospitalreservation_asklepios/Controller/HomeController.java
+++ src/main/java/com/asklepios/hospitalreservation_asklepios/Controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,6 +22,8 @@ public class HomeController {
     public String main(){
         return "home";
     }
+
+
     @GetMapping("/bboard_health")
     public String board_health(Model model, @ModelAttribute PageVO pagevo) throws Exception {
         if(pagevo.getPage()==null){
@@ -28,23 +31,41 @@ public class HomeController {
         }
         pagevo.setTotalCount(boardService.boardCount());
         List<BoardVO> boardlist=boardService.boardHealthList(pagevo);
+        System.out.println(pagevo.getPage());
+        System.out.println(pagevo.getStartNo()+"/"+pagevo.getEndNo());
+        System.out.println(pagevo.isNext());
+        System.out.println(pagevo.isPrev());
+
         model.addAttribute("boardlist",boardlist);
         return "board/main";
     }
     @GetMapping("/bboard_campaign")
-    public String board_cam(Model model, @ModelAttribute PageVO pagevo ) throws Exception {
-        List<BoardVO> boardlist=boardService.boardCampaignList( pagevo);
+    public String board_cam(Model model, @ModelAttribute PageVO pageVO ) throws Exception {
+        if(pageVO.getPage()==null){
+            pageVO.setPage(1);
+        }
+        pageVO.setTotalCount(boardService.boardCount());
+        List<BoardVO> boardlist=boardService.boardCampaignList( pageVO);
+
         model.addAttribute("boardlist",boardlist);
         return "board/main";
     }
     @GetMapping("/bboard_med")
     public String board_med(Model model , @ModelAttribute PageVO pagevo) throws Exception {
+        if(pagevo.getPage()==null){
+            pagevo.setPage(1);
+        }
+        pagevo.setTotalCount(boardService.boardCount());
         List<BoardVO> boardlist=boardService.boardMedList( pagevo);
         model.addAttribute("boardlist",boardlist);
         return "board/main";
     }
     @GetMapping("/bboard_free")
     public String board_free(Model model , @ModelAttribute PageVO pagevo) throws Exception {
+        if(pagevo.getPage()==null){
+            pagevo.setPage(1);
+        }
+        pagevo.setTotalCount(boardService.boardCount());
         List<BoardVO> boardlist=boardService.boardFreeList(pagevo);
         model.addAttribute("boardlist",boardlist);
         return "board/main";
@@ -57,8 +78,22 @@ public class HomeController {
     @PostMapping("/bboard/submitwrite")
     public String submitWrite(@ModelAttribute BoardVO boardVO) throws Exception {
         boardService.addBoard(boardVO);
-        return "redirect:/bboard";
+        return "redirect:/bboard_health";
 
     }
-
+    @GetMapping("/modboard")
+    public String mod(@RequestParam("no") String no,
+                      Model model) throws Exception {
+//        System.out.println(no);
+        BoardVO boardvo=boardService.modBoard(no);
+//        System.out.println(boardvo.getBoard_content());
+        model.addAttribute("boardVO",boardvo);
+        return "board/modwrite";
+    }
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardVO boardVO) throws Exception {
+//        System.out.println(boardVO.getBoard_category());
+        boardService.modBoard(boardVO);
+        return "redirect:/bboard_health";
+    }
 }
