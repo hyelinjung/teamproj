d64de17a2fa6f4e381ef35befa464269a9196a03
Jungon-Jo <jojungon@gmail.com>
2025-01-31 15:27:23 +0900

    Delete src directory

--- src/main/java/com/asklepios/hospitalreservation_asklepios/Service/IF_RegistrationService.java
+++
@@ -1,8 +0,0 @@
-package com.asklepios.hospitalreservation_asklepios.Service;
-
-import com.asklepios.hospitalreservation_asklepios.VO.HospitalVO;
-
-public interface IF_RegistrationService {
-  boolean duplicateCheck(String hospitalAddress, String hospitalName);
-  void registerHospital(HospitalVO hospitalvo);
-}
